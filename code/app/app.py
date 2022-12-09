fcbd64f320db4e6bee882cb9074f8f99323d4628
Gameboy612 <64597724+Gameboy612@users.noreply.github.com>
2022-12-10 00:02:29 +0800

    added comment documentation

--- code/app/app.py
+++ code/app/app.py
@@ -1,12 +1,14 @@
 import os
 import shutil
 
+# Get the current repository (so that it can run in VS code run button)
 workspace = __file__.replace('\\', '/').replace('/app.py', '')
 
 INPUT_DIRECTORY = workspace + '/input/'
 OUTPUT_DIRECTORY = workspace + '/output/'
 
 
+# Empties the output directory
 def reset_directory():
     try:
         shutil.rmtree(OUTPUT_DIRECTORY)
@@ -15,7 +17,7 @@ def reset_directory():
     os.mkdir(OUTPUT_DIRECTORY)
     return
 
-
+# Convert the dir to text via pytesseract
 def convert(dir, file = ""):
     import pytesseract
     from PIL import Image
@@ -27,6 +29,8 @@ def convert(dir, file = ""):
     f.write(text)
     f.close()
 
+
+# Loop through all the directories
 def loop_directories():
     for subdir, dirs, files in os.walk(INPUT_DIRECTORY):
         for file in files:
@@ -34,6 +38,7 @@ def loop_directories():
                 convert(os.path.join(subdir, file), file)
 
 
+# Main function
 def main():
     reset_directory()
     loop_directories()
